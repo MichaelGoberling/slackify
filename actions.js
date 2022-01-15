415074dcafb7a824b22a0a248264fe4707bbae95
Michael Goberling <michael.goberling@gmail.com>
2022-01-14 19:26:36 -0600

    Format all files

--- actions.js
+++ actions.js
@@ -1,23 +1,21 @@
-let prompts = require('./prompts');
+let prompts = require("./prompts");
 
 const getChannels = async () => {
-    return [
-        'developer-tools'
-    ]
+  return ["developer-tools"];
 };
 
 const selectChannel = async (channels) => {
-    return await prompts.selectChannelToSendMessageTo(channels);
+  return await prompts.selectChannelToSendMessageTo(channels);
 };
 
 const sendMessageToChannel = async (channel) => {
-    let { message } = await prompts.requestMessageToSend();
-    console.log(`Sending the following message to ${channel}:\n`);
-    console.log(message, "\n");
+  let { message } = await prompts.requestMessageToSend();
+  console.log(`Sending the following message to ${channel}:\n`);
+  console.log(message, "\n");
 };
 
 module.exports = {
-    getChannels, 
-    selectChannel,
-    sendMessageToChannel
-}
+  getChannels,
+  selectChannel,
+  sendMessageToChannel,
+};
--- prompts.js
+++ prompts.js
@@ -6,38 +6,34 @@ const mainMenu = () => {
       name: "actions",
       message: "What would you like to do?",
       type: "list",
-      choices: [
-        "List channels",
-        "Send a message to a channel",
-        "Quit",
-      ],
-    }
+      choices: ["List channels", "Send a message to a channel", "Quit"],
+    },
   ]);
 };
 
 const selectChannelToSendMessageTo = (channels) => {
-    return inquirer.prompt([
-        {
-            name: "channel",
-            message: "Which channel would you like to send a message to?",
-            type: "list",
-            choices: channels
-        }
-    ]);
-}
+  return inquirer.prompt([
+    {
+      name: "channel",
+      message: "Which channel would you like to send a message to?",
+      type: "list",
+      choices: channels,
+    },
+  ]);
+};
 
 const requestMessageToSend = () => {
-    return inquirer.prompt([
-        {
-            name: "message",
-            message: "What would you like to send to the channel?",
-            type: "input"
-        }
-    ]);
-}
+  return inquirer.prompt([
+    {
+      name: "message",
+      message: "What would you like to send to the channel?",
+      type: "input",
+    },
+  ]);
+};
 
 module.exports = {
-    mainMenu,
-    selectChannelToSendMessageTo,
-    requestMessageToSend
+  mainMenu,
+  selectChannelToSendMessageTo,
+  requestMessageToSend,
 };
