27dddaf9231a0645121d87d9c9bed3fedd03824a
Michael Goberling <michael.goberling@gmail.com>
2022-01-14 20:02:14 -0600

    Initial Slack API integration, add actions: list channels, send message to channel

--- actions.js
+++ actions.js
@@ -1,17 +1,45 @@
 let prompts = require("./prompts");
+let slack = require("./slack");
 
+/**
+ * List channels in the workspace the current access token is associated with.
+ * @returns {string[]}
+ */
 const getChannels = async () => {
-  return ["developer-tools"];
+  try {
+    return await slack.listChannels();
+  } catch (err) {
+    console.error(err);
+  }
 };
 
+/**
+ * Prompt the user to select a channel out of a list of provided channels. 
+ * @param {string[]} channels 
+ * @returns {string}
+ */
 const selectChannel = async (channels) => {
-  return await prompts.selectChannelToSendMessageTo(channels);
+  try {
+    return await prompts.selectChannelToSendMessageTo(channels);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
+/**
+ * Prompt the user for a message to send to a channel. Send the message to a channel.
+ * @param {string} channel 
+ */
 const sendMessageToChannel = async (channel) => {
-  let { message } = await prompts.requestMessageToSend();
-  console.log(`Sending the following message to ${channel}:\n`);
-  console.log(message, "\n");
+  try {
+    let { message } = await prompts.requestMessageToSend();
+    console.log(`Sending the following message to ${channel}:\n`);
+    console.log(message, "\n");
+    await slack.sendMessageToChannel(channel, message);
+    console.log("Message sent.\n");
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 let prompts = require("./prompts");
 let actions = require("./actions");
+require("dotenv").config();
 
 (async () => {
   console.clear();
 
-  while (true) {
-    let response = await prompts.mainMenu();
-    let channels;
-    switch (response.actions) {
-      case "List channels":
-        channels = await actions.getChannels();
-        channels.forEach((channel) => console.log(channel));
-        break;
-      case "Send a message to a channel":
-        channels = await actions.getChannels();
-        let { channel } = await actions.selectChannel(channels);
-        await actions.sendMessageToChannel(channel);
-        break;
-      case "Quit":
-        process.exit();
-      default:
-        break;
+  let channels;
+  try {
+    while (true) {
+      let response = await prompts.mainMenu();
+      switch (response.actions) {
+        case "List channels":
+          channels = await actions.getChannels();
+          channels.forEach((channel) => console.log(channel));
+          break;
+
+        case "Send a message to a channel":
+          channels = await actions.getChannels();
+          let { channel } = await actions.selectChannel(channels);
+          await actions.sendMessageToChannel(channel);
+          break;
+
+        case "Quit":
+          process.exit();
+
+        default:
+          break;
+      }
     }
+  } catch (err) {
+    console.error(err);
   }
 })();
